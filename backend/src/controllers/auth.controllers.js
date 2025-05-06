81ae64416e060021442cc46a8661869265dc709b
Somnath Dey <somnath.dey@aprodence.com>
2025-05-06 23:29:53 +0530

    feat(auth): add logout controller

--- backend/src/controllers/auth.controllers.js
+++ backend/src/controllers/auth.controllers.js
@@ -111,5 +111,25 @@ export const login = async (req, res) => {
     });
   }
 };
-export const logout = async (req, res) => {};
+
+export const logout = async (req, res) => {
+  try {
+    res.clearCookie("jwt", {
+      httpOnly: true,
+      sameSite: "strict",
+      secure: process.env.NODE_ENV !== "development",
+    });
+
+    res.status(200).json({
+      success: true,
+      message: "User logged out successfully",
+    });
+  } catch (error) {
+    console.error("Error logging out user:", error);
+    res.status(500).json({
+      error: "Error looging out user",
+    });
+  }
+};
+
 export const check = async (req, res) => {};
