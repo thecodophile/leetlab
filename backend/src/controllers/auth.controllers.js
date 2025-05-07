550a459115bd3f7334ff8a9d9cfd62569db49455
Somnath Dey <somnath.dey@aprodence.com>
2025-05-08 00:02:47 +0530

    feat(auth): add auth middleware, and me controller

--- backend/src/controllers/auth.controllers.js
+++ backend/src/controllers/auth.controllers.js
@@ -132,4 +132,17 @@ export const logout = async (req, res) => {
   }
 };
 
-export const check = async (req, res) => {};
+export const me = async (req, res) => {
+  try {
+    res.status(200).json({
+      success: true,
+      message: "User authenticated successfully",
+      user: req.user,
+    });
+  } catch (error) {
+    console.error("Error checking user:", error);
+    res.status(500).json({
+      error: "Error checking user",
+    });
+  }
+};
--- backend/src/routes/auth.routes.js
+++ backend/src/routes/auth.routes.js
@@ -1,16 +1,17 @@
 import express from "express";
 import {
-  check,
+  me,
   login,
   logout,
   register,
 } from "../controllers/auth.controllers.js";
+import { authMiddleware } from "../middlewares/auth.middleware.js";
 
 const authRoutes = express.Router();
 
 authRoutes.post("/register", register);
 authRoutes.post("/login", login);
-authRoutes.post("/logout", logout);
-authRoutes.get("/check", check);
+authRoutes.post("/logout", authMiddleware, logout);
+authRoutes.get("/me", authMiddleware, me);
 
 export default authRoutes;
