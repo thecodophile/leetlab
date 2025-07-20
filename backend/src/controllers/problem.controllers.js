c067bfdfb1968dde6baf091aeac93082a2801eb0
Somnath Dey <sdey.aklakhi@gmail.com>
2025-07-21 00:55:47 +0530

    feat(problem): implement updateProblem controller to allow problem updates

--- backend/src/controllers/problem.controllers.js
+++ backend/src/controllers/problem.controllers.js
@@ -133,7 +133,98 @@ export const getProblemById = async (req, res) => {
   }
 };
 
-export const updateProblem = async (req, res) => {};
+export const updateProblem = async (req, res) => {
+  const { id } = req.params;
+  // get all the data from the req body
+  const {
+    title,
+    description,
+    difficulty,
+    tags,
+    examples,
+    constraints,
+    testcases,
+    codeSnippets,
+    referenceSolution,
+  } = req.body;
+
+  // going to check the user role once again
+  if (req.user.role !== "ADMIN") {
+    return res.status(403).json({
+      error: "You are not allowed to update a problem",
+    });
+  }
+
+  const problem = await db.problem.findUnique({
+    where: {
+      id,
+    },
+  });
+
+  if (!problem) {
+    return res.status(404).json({
+      error: "Problem can not found",
+    });
+  }
+
+  try {
+    // loop through each reference solutions for different languages
+    for (const [language, solutionCode] of Object.entries(referenceSolution)) {
+      const languageId = getJudge0LanguageId(language);
+
+      if (!languageId) {
+        return res.status(400).json({
+          error: `Language ${language} is not supported`,
+        });
+      }
+
+      const submissions = testcases.map(({ input, output }) => ({
+        source_code: solutionCode,
+        language_id: languageId,
+        stdin: input,
+        expected_output: output,
+      }));
+
+      const submissionsResults = await submitBatch(submissions);
+
+      const tokens = submissionsResults.map((res) => res.token);
+
+      const results = await pollBatchResults(tokens);
+
+      for (let i = 0; i < results.length; i++) {
+        const result = results[i];
+
+        if (result.status.id !== 3) {
+          return res.status(400).json({
+            error: `Testcase ${i + 1} failed for language ${language}`,
+          });
+        }
+      }
+    }
+    const newProblem = await db.problem.update({
+      where: {
+        id,
+      },
+      data: {
+        title,
+        description,
+        difficulty,
+        tags,
+        examples,
+        constraints,
+        testcases,
+        codeSnippets,
+        referenceSolution,
+        userId: req.user.id,
+      },
+    });
+
+    res.status(200).json({ success: true, problem: newProblem });
+  } catch (error) {
+    console.log("update problem error ->", error);
+    res.status(400).json({ success: false, error });
+  }
+};
 
 export const deleteProblem = async (req, res) => {};
 
